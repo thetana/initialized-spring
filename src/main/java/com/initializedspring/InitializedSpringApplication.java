6e1a8b5f17109ce4d3c0102ced8c1b770dc22486
thetana <zxsa135@naver.com>
2026-02-13 14:46:03 +0900

    Feat: 첫 커밋

---
+++ src/main/java/com/initializedspring/InitializedSpringApplication.java
@@ -0,0 +1,13 @@
+package com.initializedspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializedSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializedSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/initializedspring/InitializedSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initializedspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializedSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
