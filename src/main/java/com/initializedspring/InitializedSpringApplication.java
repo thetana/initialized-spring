04258ac8c730e87edea62456e3fe35725c4471f5
thetana <zxsa135@naver.com>
2026-02-13 17:25:00 +0900

    Feat: 첫 커밋222

--- src/main/java/com/initializedspring/InitializedSpringApplication.java
+++ src/main/java/com/initializedspring/InitializedSpringApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InitializedSpringApplication {
 
+    //수정 작업중
     public static void main(String[] args) {
         SpringApplication.run(InitializedSpringApplication.class, args);
-    }
+    } // 수정중
 
 }
---
+++ src/main/java/com/initializedspring/javaqwerq.java
@@ -0,0 +1,5 @@
+package com.initializedspring;
+
+public class javaqwerq {
+    int a;
+}
