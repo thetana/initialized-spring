a9f294fd4e1b65a60a00dfd20e1eaa8eac102908
puddingdream <edcrfv098@naver.com>
2026-02-13 17:26:48 +0900

    feat: userEntity

--- src/main/java/com/initializedspring/InitializedSpringApplication.java
+++ src/main/java/com/initializedspring/InitializedSpringApplication.java
@@ -10,4 +10,6 @@ public class InitializedSpringApplication {
         SpringApplication.run(InitializedSpringApplication.class, args);
     } // 수정중qweqwe
 
+
+
 }
---
+++ src/main/java/com/initializedspring/user/entity/User.java
@@ -0,0 +1,17 @@
+package com.initializedspring.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Table
+@RequiredArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
